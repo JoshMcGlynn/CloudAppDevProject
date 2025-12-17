5d9c5b56e4a1d50b1409e81004fb66be95cbbaf4
Josh McGlynn <x21358616@student.ncirl.ie>
2025-12-17 04:25:09 +0000

    Got rid of more solid queue code in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Disable solid queue
   config.active_job.queue_adapter = :async
-  
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
 
@@ -53,8 +53,6 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
